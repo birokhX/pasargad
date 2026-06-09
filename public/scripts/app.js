692cc1424c5401bbb261f4997a77aeaa301844e6
birokhX <birokhx@gmail.com>
2026-06-09 15:29:27 -0700

    feat: creating (notification)

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -7,60 +7,94 @@ openNavMobileBtn.addEventListener("click", openNav);
 closeNavMobileBtn.addEventListener("click", closeNav);
 
 function closeNav() {
-    navMobile.classList.remove("right-2");
-    navMobile.classList.add("-right-64");
-    setTimeout(function () {
-        overlay.classList.remove("overlay--visible");
-
-    }, 200)
+  navMobile.classList.remove("right-2");
+  navMobile.classList.add("-right-64");
+  setTimeout(function () {
+    overlay.classList.remove("overlay--visible");
+  }, 200);
 }
 function openNav() {
-    navMobile.classList.remove("-right-64");
-    navMobile.classList.add("right-2");
-    overlay.classList.add("overlay--visible");
+  navMobile.classList.remove("-right-64");
+  navMobile.classList.add("right-2");
+  overlay.classList.add("overlay--visible");
 }
 overlay.addEventListener("click", closeNav);
 
 // Alpine
-document.addEventListener('alpine:init', () => {
-    Alpine.data('accordianGroup', () => ({
-        activeItem: null,
-        toggle(id) {
-            this.activeItem = this.activeItem === id ? null : id
-        },
-        isOpen(id) {
-            return this.activeItem === id
-        }
-    }))
+document.addEventListener("alpine:init", () => {
+  Alpine.data("accordianGroup", () => ({
+    activeItem: null,
+    toggle(id) {
+      this.activeItem = this.activeItem === id ? null : id;
+    },
+    isOpen(id) {
+      return this.activeItem === id;
+    },
+  }));
+  //   Dropdown
+  Alpine.data("dropdown", (openItem = false) => ({
+    open: openItem,
+    communicationCard: false,
+    communicationItem: null,
+
+    communicationCardToggle() {
+      this.communicationCard = !this.communicationCard;
+    },
+    communicationCardClose() {
+      this.communicationCard = false;
+    },
+    getCommunicationIcon() {
+      if (this.communicationItem === 1) {
+        return "#telegram";
+      } else if (this.communicationItem === 2) {
+        return "#bale";
+      } else {
+        return "#eita";
+      }
+    },
+    getCommunicationText() {
+      if (this.communicationItem === 1) {
+        return "@birokhx";
+      } else if (this.communicationItem === 2) {
+        return "@birokhx";
+      } else {
+        return "@birokhx";
+      }
+    },
+
+    close() {
+      this.open = false;
+      document.body.classList.remove("overflow-hidden");
+    },
+  }));
 });
 
 // Swiper
-document.querySelectorAll('.js-swiper').forEach(element => {
-    const type = element.dataset.type
-    if (type === 'managers') {
-        new Swiper(element, {
-            slidesPerView: 1,
-            loop: true,
-            autoplay:true,
-            breakpoints: {
-                640: {
-                    slidesPerView: 2,
-                    spaceBetween: 15
-                },
-                768: {
-                    slidesPerView: 2,
-                    spaceBetween: 20
-
-                },
-                1024: {
-                    slidesPerView: 3,
-                    spaceBetween: 20
-                },
-                1280: {
-                    slidesPerView: 4,
-                    spaceBetween: 20
-                }
-            }
-        })
-    }
-})
+document.querySelectorAll(".js-swiper").forEach((element) => {
+  const type = element.dataset.type;
+  if (type === "managers") {
+    new Swiper(element, {
+      slidesPerView: 1,
+      loop: true,
+      autoplay: true,
+      breakpoints: {
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 15,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        1024: {
+          slidesPerView: 3,
+          spaceBetween: 20,
+        },
+        1280: {
+          slidesPerView: 4,
+          spaceBetween: 20,
+        },
+      },
+    });
+  }
+});
